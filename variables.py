466163aee62b781d2cb49ff90fa1a71ddcebe394
Ontiveros Alfaro Johán Alejandro <111895521+ChemsJam@users.noreply.github.com>
2023-12-25 17:43:01 -0600

    uso de variables

---
+++ variables.py
@@ -0,0 +1,13 @@
+x, book = 100, "I Robot"
+
+print(x,book)
+
+#Conventiones
+#O formas de definir variables
+book_name ="I Robot" #Snake Case
+bookName = "Digital fortress" #Came Case
+BookName = "asdaasdsadas" #PascalCase
+
+#las variables pueden cambiar de tipo de dato, desde string o integer, hasta una lista o tupla
+book_name ="I Robot"
+book_name =12313213213
